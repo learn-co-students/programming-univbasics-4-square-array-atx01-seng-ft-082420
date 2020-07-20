d29e6cc0dbc04711ab9c879ebdcf5d2e746bfc8a
Ciara Picou <ciarapicou@att.net>
2020-07-20 18:09:51 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,35 @@
+# def square_array(array)
+# new_array = []
+# array.length.times do |i|
+#   new_array.push(array[i]*array[i])
+# end
+#   return new_array
+# end
+
 def square_array(array)
 new_array = []
 array.length.times do |i|
-  new_array.push(array[i]*array[i])
+  new_array.push(array[i]**2)
 end
-  return new_array
+new_array
 end
+
+
+# def square_array(array)
+#   counter = 0
+# new_array = []
+# while array[counter] do
+# new_array.push(array[counter] * array[counter])
+#   counter += 1
+# end
+# return new_array
+# end
+
+# def square_array(array)
+# new_array = []
+# array.length.times { |index|
+#   new_array.push(array[index] * array[index])
+# }
+#   return new_array
+# end
+
